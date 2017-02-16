230920f8003da36af9b12d866d993b8bdd60497b
LL <NZLILI2015@gmail.com>
2017-02-17 00:45:31 +1300

    update the algorithms

--- ForDrives.Services/Core/InterpretationService.cs
+++ ForDrives.Services/Core/InterpretationService.cs
@@ -15,26 +15,26 @@ namespace ForDrives.Services.Core
 
             string binaryString = Convert.ToString(number, 2).PadLeft(26, '0');
             var reversedBinaryStr = binaryString.Reverse().ToList();
-            for (int i = 0; i < reversedBinaryStr.Count; i++)            
+            for (int i = 0; i < reversedBinaryStr.Count; i++)
                 result += reversedBinaryStr[i] == '1' ? ((char)(i + 65)).ToString() : "";
-            
+
             return result;
         }
-
+        
         public int ToNumber(string letters)
         {
-            int result = 0;
-            bool[] CHS = new bool[27];
-            for (int i = 0; i <= letters.Length - 1; i++)
+            Func<int, int> pow2 = delegate (int x)
             {
-                if (((byte)letters[i] < 65) | ((byte)letters[i] > 90))
-                    continue;
-                else
-                    CHS[(byte)letters[i] - 64] = true;
+                return (int)Math.Pow(2, x);
+            };
+
+            int result = 0;
+            for (int i = 0; i < letters.Length; i++)
+            {
+                var invalid = (letters[i] < 65) || (letters[i] > 90);
+                result += invalid ? 0 : pow2(letters[i] - 65);                
             }
-            for (int i = 1; i <= 26; i++)
-                if (CHS[i])
-                    result += (int)Math.Pow(2, (i - 1));
+
             return result;
         }
     }
