151b608c1791ee074322ab81ff7d28353565bd9a
LL <cgloier@163.com>
2017-02-17 15:30:49 +1300

    Now implemented part of the ViewModel

--- ForDrives/ViewModels/MainViewModel.cs
+++ ForDrives/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ForDrives.Services.Core;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -9,8 +10,14 @@ namespace ForDrives.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        public MainViewModel()
+        {
+            applicationTitle = "ForDrives";
+            ReloadSettings();
+        }
+
         private string applicationTitle;
-        
+
         public string ApplicationTitle
         {
             get { return applicationTitle; }
@@ -24,9 +31,70 @@ namespace ForDrives.ViewModels
             }
         }
 
-        public MainViewModel()
+        private void ReloadSettings()
         {
-            applicationTitle = "ForDrives";
+            currentSetting_Visibility = new DrivesVisibilityService().GetCurrentSettings();
+            currentSetting_Accessibilty = new DrivesAccessibilityService().GetCurrentSettings();
+        }
+
+        private string currentSetting_Accessibilty;
+
+        public string CurrentSetting_Accessibilty
+        {
+            get
+            {
+                return currentSetting_Accessibilty;
+            }
+            set
+            {
+                currentSetting_Accessibilty = value;
+                OnPropertyChanged("DrivesAccessibiltyCurrentSetting");
+            }
+        }
+
+        private string currentSetting_Visibility;
+
+        public string CurrentSetting_Visibility
+        {
+            get
+            {
+                return currentSetting_Visibility;
+            }
+            set
+            {
+                currentSetting_Visibility = value;
+                OnPropertyChanged("CurrentSetting_Visibility");
+            }
+        }
+
+        private string newSetting_Visibility;
+
+        public string NewSetting_Visibility
+        {
+            get
+            {
+                return newSetting_Visibility;
+            }
+            set
+            {
+                newSetting_Visibility = value;
+                OnPropertyChanged("NewSetting_Visibility");
+            }
+        }
+
+        private string newSetting_Accessibility;
+
+        public string NewSetting_Accessibility
+        {
+            get
+            {
+                return newSetting_Accessibility;
+            }
+            set
+            {
+                newSetting_Accessibility = value;
+                OnPropertyChanged("NewSetting_Accessibility");
+            }
         }
     }
 }
