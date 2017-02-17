ced563df5027f7dde05a35d6dbf3d6451b5071fc
LL <cgloier@163.com>
2017-02-17 15:50:48 +1300

    Update assembly version

--- ForDrives/Properties/AssemblyInfo.cs
+++ ForDrives/Properties/AssemblyInfo.cs
@@ -51,8 +51,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: Guid("99f16fe9-4282-4669-bf44-3010f5d6c9b9")]
 
